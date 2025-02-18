fbb3d285f52f84bb27a718bb62edf392e0e0a460
TEEPERUNA <eki.forssell@gmail.com>
2025-02-18 17:38:08 +0200

    Created Car class with getCarInfo method

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ console.log(doubled);
 
 const filtered = num.filter(n => n > 2);
 console.log(filtered);
+
+class car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    getCarInfo() {
+    return `${this.make} ${this.model} ${this.year}`;
+    }
+
+}
+
+const car1 = new Car("Toyota", "Corolla", 2021);
+const car2 = new Car("Honda", "Civic", 2022);
+
+console.log(car1.getCarInfo());
+console.log(car2.getCarInfo());
