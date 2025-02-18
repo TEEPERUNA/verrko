028cdc96fd14f87110cd842a61e94b35a9ff71a6
TEEPERUNA <eki.forssell@gmail.com>
2025-02-18 17:32:06 +0200

    Implemented array operations using map and filter with arrow functions

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ function compareNumbers(a, b) {
 
 calculateArea(5);
 compareNumbers(10, 5);
+
+const num = [1, 2, 3, 4, 5];
+const doubled = num.map(n => n * 2);
+console.log(doubled);
+
+const filtered = num.filter(n => n > 2);
+console.log(filtered);
