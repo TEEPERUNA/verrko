f0fd98c0520ae3fa6b3ff54574f3c198e2e52a70
Erik <eki.forssell@gmail.com>
2025-03-19 22:43:02 +0200

    Added LoginMessage

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Greeting from "./components/greetings";
 import ThemeToggle from "./components/ThemeToggle";
+import LoginMessage from "./components/LoginMessage";
 
 const App = () => (
 
     <div>
       <Greeting name="Erik" />
+      <LoginMessage isLoggedIn={true} />
       <ThemeToggle />
     </div>
 
---
+++ first-app/src/components/LoginMessage.jsx
@@ -0,0 +1,4 @@
+export default function LoginMessage({ isLoggedIn }) {
+    return <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>;
+  }
+  
