b27e13a4a9980c67d5c8ede95dd2dffa65a13ae8
Erik <eki.forssell@gmail.com>
2025-03-20 04:53:36 +0200

    updated styling

--- tic-tac-toe/src/Square.jsx
+++ tic-tac-toe/src/Square.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './App.css'
 
 function Square({ value, onSquareClick }) {
+  // Choose a color based on the value
+  let style = { color: '#333' }
+  if (value === 'X') style.color = '#e74c3c' // red for X
+  if (value === 'O') style.color = '#3498db' // blue for O
+
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="square" onClick={onSquareClick} style={style}>
       {value}
     </button>
   )
